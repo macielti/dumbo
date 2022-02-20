eaf38498e724d3892f9f29b60500e5a5403ce3d4
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-02-20 01:23:55 -0300

    add pre application internal models schemas

--- src/dumbo/models/application.clj
+++ src/dumbo/models/application.clj
@@ -1,4 +1,20 @@
 (ns dumbo.models.application
-  (:require [dumbo.wire.datomic.application :as wire.datomic.application]))
+  (:require [schema.core :as s]
+            [dumbo.wire.datomic.application :as wire.datomic.application]
+            [schema.experimental.abstract-map :as abstract-map]))
 
-(def Application wire.datomic.application/Application)
+(def PreApplicationType (->> wire.datomic.application/application-types
+                             (apply s/enum)))
+
+(def base-pre-application
+  {:pre-application/type PreApplicationType})
+
+(s/defschema PreApplication (abstract-map/abstract-map-schema :type
+                                                              base-pre-application))
+
+(abstract-map/extend-schema YoutubePreApplication PreApplication [:youtube]
+  {:pre-application/access-code s/Str})
+
+(abstract-map/extend-schema RedditPreApplication PreApplication [:reddit]
+  {:pre-application/access-token  s/Str
+   :pre-application/refresh-token s/Str})
