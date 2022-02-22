a507f28387cc9836e0cb7427e33a178d65ee1a32
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2022-02-22 13:15:03 -0300

    remove unused logic function

--- src/dumbo/logic/application.clj
+++
@@ -1,8 +0,0 @@
-(ns dumbo.logic.application
-  (:require [schema.core :as s]
-            [dumbo.models.application :as models.application]))
-
-(s/defn expired? :- s/Bool
-  [{:application/keys [access-expires-at]} :- models.application/Application
-   now :- s/Inst]
-  (.after now access-expires-at))
